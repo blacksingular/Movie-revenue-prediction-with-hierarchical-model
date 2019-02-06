1386435ff2a6bd63693256b2ce48e5a1d5729fec
blacksingular <35845243+blacksingular@users.noreply.github.com>
2019-02-06 15:57:49 -0500

    02061557

--- knowledge_graph.py
+++ knowledge_graph.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 
-import matplotlib.pyplot as plt; plt.rcdefaults()
+import matplotlib.pyplot as plt;
+
+plt.rcdefaults()
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.ticker import FuncFormatter
@@ -10,6 +12,7 @@ import pandas as pd
 import ast
 import csv
 import matplotlib
+
 matplotlib.use('agg')
 
 
@@ -25,19 +28,19 @@ class KnowledgeGraph:
         self.trash = []
         self.chosen_list = []
         self.user_movielist_dict = {}
-        self.actor_node_type = {} # this is the dict of node:type
+        self.actor_node_type = {}  # this is the dict of node:type
         self.director_node_type = {}
         self.movie_node_type = {}
         self.genre_node_type = {}
         self.user_node_type = {}
-        self.edge_type = [] # this is the dict of edge:type
+        self.edge_type = []  # this is the dict of edge:type
         self.year_movie = {}
-        self.chosen_train_dict = {} # restore the movie_id between 2005 - 2013
-        self.chosen_test_dict = {} # restore the movie_id between 2014 - 2015
-        self.md_relation_tr = {} # train data of director and movies relationship
-        self.md_relation_te = {} # test
-        self.mf_relation = {} # movie and features relationship
-        self.revenues = {} # restore average revenue of a director.
+        self.chosen_train_dict = {}  # restore the movie_id between 2005 - 2013
+        self.chosen_test_dict = {}  # restore the movie_id between 2014 - 2015
+        self.md_relation_tr = {}  # train data of director and movies relationship
+        self.md_relation_te = {}  # test
+        self.mf_relation = {}  # movie and features relationship
+        self.revenues = {}  # restore average revenue of a director.
 
     def data_cleaning(self):
         #  filter the movie from 2005-2015
@@ -72,20 +75,18 @@ class KnowledgeGraph:
                 # if dic['order'] < 3:
                 # self.actor_nodes.append([movie_id, dic['id'], 1])
                 # self.actor_nodes.append([dic['id'], movie_id, 1])
-                self.actor_node_type[('a:'+str(dic['id']))] = 1
-                self.movie_node_type['m:'+str(movie_id)] = 1
-                self.edge_type.append('a:'+str(dic['id'])+' '+'m:'+str(movie_id)+' '+'1 a')
+                self.actor_node_type[('a:' + str(dic['id']))] = 1
+                self.movie_node_type['m:' + str(movie_id)] = 1
+                self.edge_type.append('a:' + str(dic['id']) + ' ' + 'm:' + str(movie_id) + ' ' + '1 a')
         # pdb.set_trace()
         # with open(movie_actor_path, 'w') as f:
         #     for edge in self.actor_nodes:
         #         f.write(str(edge[0]) + ',' + str(edge[1]) + "\n")
 
-
         # writing
         # with open(movie_actor_path, 'w') as csvfile:
         #     csv.writer(csvfile).writerow(['movie_id', 'actor'])
         #     csv.writer(csvfile, delimiter='\t').writerows(self.actor_nodes)
-
 
         # plot
         # self.actor_dict = dict()
@@ -142,8 +143,8 @@ class KnowledgeGraph:
                 if dic['job'] == 'Director':
                     # self.director_nodes.append([movie_id, dic['id'],1])
                     # self.director_nodes.append([dic['id'],movie_id,1])
-                    self.director_node_type[('d:'+str(dic['id']))] = 1
-                    self.movie_node_type['m:'+str(movie_id)] = 1
+                    self.director_node_type[('d:' + str(dic['id']))] = 1
+                    self.movie_node_type['m:' + str(movie_id)] = 1
                     self.edge_type.append('d:' + str(dic['id']) + ' ' + 'm:' + str(movie_id) + ' ' + '1 d')
 
         # writing
@@ -153,7 +154,6 @@ class KnowledgeGraph:
         # with open(movie_director_path, 'w') as csvfile:
         #     csv.writer(csvfile).writerow(['movie_id', 'director','weight'])
         #     csv.writer(csvfile,delimiter='\t').writerows(self.director_nodes)
-
 
         # plot
         # self.direct_dict = dict()
@@ -198,7 +198,7 @@ class KnowledgeGraph:
         # export movie_genre csv
         year_nummovies = {}
         genre_df = pd.DataFrame(pd.read_csv(genre_path, low_memory=False))
-        for row in genre_df[['id', 'genres', 'title','release_date']].iterrows():
+        for row in genre_df[['id', 'genres', 'title', 'release_date']].iterrows():
             movie_id = row[1]['id']
             # if movie_id in self.trash or str(movie_id) not in self.chosen_list:
             #     continue
@@ -208,13 +208,13 @@ class KnowledgeGraph:
             for dic in ast.literal_eval(row[1]['genres']):
                 genre_id = dic['id']
                 genre_name = dic['name']
-                if '2005' <= release_year <= '2013':
+                if '2005' <= release_year <= '2015':
                     # self.genre_nodes.append([movie_id, genre_id, 1])
                     # self.genre_nodes.append([genre_id, movie_id, 1])
-                    self.genre_node_type[('g:'+str(genre_id))] = 1
-                    self.movie_node_type['m:'+str(movie_id)] = 1
+                    self.genre_node_type[('g:' + str(genre_id))] = 1
+                    self.movie_node_type['m:' + str(movie_id)] = 1
                     self.edge_type.append('g:' + str(genre_id) + ' ' + 'm:' + str(movie_id) + ' ' + '1 g')
-                
+
         #         if release_year not in self.year_movie:
         #             movie_list = []
         #             movie_list.append(movie_id)
@@ -247,40 +247,39 @@ class KnowledgeGraph:
             csv.writer(csvfile).writerow(['movie_id', 'movie_title', 'genre_id', 'genre'])
             csv.writer(csvfile, delimiter='\t').writerows(self.genre_nodes)
         # plot the distribution of the genre
-        
-        
-#        genre_dict = dict()
-#        for instance in self.genre_nodes:
-#            if instance[3] not in genre_dict:
-#                movie_list = []
-#                movie_list.append(instance[1])
-#                genre_dict[instance[3]] = movie_list
-#            else:
-#                movie_list = genre_dict[instance[3]]
-#                if instance[1] not in movie_list:
-#                    movie_list.append(instance[1])
-#                    genre_dict[instance[3]] = movie_list
-#        # pie of distribution
-#        genre_list= []
-#        num_list = []
-#        for genre, movie_list in genre_dict.items():
-#            genre_dict[genre] = len(movie_list)
-#        genre_sorted = sorted(genre_dict.items(), key=lambda kv: kv[1])
-#        for genre in genre_sorted:
-#            if genre[1] >1 :
-#                genre_list.append(genre[0])
-#                num_list.append(genre[1])
-##        print(num_list)
-#
-#        objects = genre_list
-#        y_pos = np.arange(len(objects))
-#        performance = num_list
-#
-#        plt.barh(y_pos, performance, align='center', alpha=0.5)
-#        plt.yticks(y_pos, objects)
-#        plt.xlabel('# Movies')
-#        plt.title('genre_distribution')
-#        plt.savefig('./output/genre_distribution.png')
+
+    #        genre_dict = dict()
+    #        for instance in self.genre_nodes:
+    #            if instance[3] not in genre_dict:
+    #                movie_list = []
+    #                movie_list.append(instance[1])
+    #                genre_dict[instance[3]] = movie_list
+    #            else:
+    #                movie_list = genre_dict[instance[3]]
+    #                if instance[1] not in movie_list:
+    #                    movie_list.append(instance[1])
+    #                    genre_dict[instance[3]] = movie_list
+    #        # pie of distribution
+    #        genre_list= []
+    #        num_list = []
+    #        for genre, movie_list in genre_dict.items():
+    #            genre_dict[genre] = len(movie_list)
+    #        genre_sorted = sorted(genre_dict.items(), key=lambda kv: kv[1])
+    #        for genre in genre_sorted:
+    #            if genre[1] >1 :
+    #                genre_list.append(genre[0])
+    #                num_list.append(genre[1])
+    ##        print(num_list)
+    #
+    #        objects = genre_list
+    #        y_pos = np.arange(len(objects))
+    #        performance = num_list
+    #
+    #        plt.barh(y_pos, performance, align='center', alpha=0.5)
+    #        plt.yticks(y_pos, objects)
+    #        plt.xlabel('# Movies')
+    #        plt.title('genre_distribution')
+    #        plt.savefig('./output/genre_distribution.png')
 
     def delete_few_people(self):
         credit_df = pd.DataFrame(pd.read_csv(credit_path))
@@ -307,18 +306,18 @@ class KnowledgeGraph:
 
     def user_rating(self):
         # we would like to see the distribution of user rating
-        #userId	movieId	rating	timestamp
+        # userId	movieId	rating	timestamp
         user_num_dict = {}
-        
+
         user_df = pd.DataFrame(pd.read_csv(user_rating_path, low_memory=False))
-        for row in user_df[['userid', 'movieid', 'rating','timestamp']].iterrows():
+        for row in user_df[['userid', 'movieid', 'rating', 'timestamp']].iterrows():
             user_id = row[1]['userid']
             movie_id = row[1]['movieid']
             if movie_id in self.trash or str(movie_id) not in self.chosen_train_dict:
                 continue
-            self.user_node_type['u:'+str(user_id)] = 1
+            self.user_node_type['u:' + str(user_id)] = 1
             self.movie_node_type['m:' + str(movie_id)] = 1
-            self.edge_type.append('u:'+str(user_id)+' '+'m:'+str(movie_id) + ' ' + '1 u')
+            self.edge_type.append('u:' + str(user_id) + ' ' + 'm:' + str(movie_id) + ' ' + '1 u')
         #     if user_id not in self.user_movielist_dict:
         #         movie_list = []
         #         movie_list.append(movie_id)
@@ -365,22 +364,23 @@ class KnowledgeGraph:
                 f.write(node)
                 f.write('\n')
 
-#        with open('./output/director.node','w') as f:
+        #        with open('./output/director.node','w') as f:
 
-        with open('./output/movie.node','w') as f:
+        with open('./output/movie.node', 'w') as f:
             for node in self.movie_node_type:
                 f.write(node)
                 f.write('\n')
-#        with open('./output/genre.node','w') as f:
+        #        with open('./output/genre.node','w') as f:
 
-        with open('./output/HIN.hin','w') as f:
+        with open('./output/HIN.hin', 'w') as f:
             for edge in self.edge_type:
                 f.write(edge)
                 f.write('\n')
 
-    def simple_baseline(self):
+    def simple_baseline_for_revenue(self):
         credit_df = pd.DataFrame(pd.read_csv(credit_path))
-        revenue_df = pd.DataFrame(pd.read_csv(genre_path, low_memory='False'))
+        revenue_df = pd.DataFrame(pd.read_csv(genre_path))
+        # user_df = pd.DataFrame(pd.read_csv(user_rating_path, low_memory=False))
         for row in credit_df[['cast', 'crew', 'id']].iterrows():
             movie_id = row[1]['id']
             if str(movie_id) in self.chosen_train_dict:
@@ -399,9 +399,16 @@ class KnowledgeGraph:
                             self.md_relation_te[dic['id']] = [movie_id]
         for row in revenue_df[['revenue', 'id']].iterrows():
             movie_id = row[1]['id']
-            if str(movie_id) in self.chosen_train_dict:
+            if str(movie_id) in self.chosen_train_dict or str(movie_id) in self.chosen_test_dict:
                 self.mf_relation[movie_id] = int(row[1]['revenue'])
-                #pdb.set_trace()
+        # for row in user_df[['userid', 'movieid', 'rating', 'timestamp']].iterrows():
+        #     movie_id = row[1]['movieid']
+        #     if str(movie_id) in self.chosen_train_dict + self.chosen_test_dict:
+        #         if movie_id in self.mf_relation:
+        #             self.mf_relation[movie_id].append(int(row[1]['rating']))
+        #         else:
+        #             self.mf_relation[movie_id] = [int(row[1]['rating'])]
+        #         # pdb.set_trace()
         for (k, v) in self.md_relation_tr.items():
             total = 0
             flag = 0
@@ -411,13 +418,26 @@ class KnowledgeGraph:
                     flag += 1
             self.revenues[k] = total / flag
         my_list = sorted(self.revenues.items(), key=lambda x: x[1])
-        for 
-    def evaluation(self):
-        pass
+        self.mean = sum(list(zip(*my_list))[1]) / len(my_list)
+        pdb.set_trace()
 
+    def evaluation(self):
+        error = 0
+        exist = 0
+        for (k, v) in self.md_relation_te.items():
+            if k in self.revenues:
+                pre = int(self.revenues[k])
+                exist += 1
+                for id in v:
+                    if str(id) in self.mf_relation:
+                        label = int(self.mf_relation[str(id)])
+                        error += (label - pre) ** 2 / (label - self.mean) ** 2
+                        # pdb.set_trace()
+        R = 1 - error
+        print(R, exist)
 
 def main():
-    KG = KnowledgeGraph() # init the KG
+    KG = KnowledgeGraph()  # init the KG
     KG.data_cleaning()
     print('data cleaning finished')
     # KG.movie_director('init')
@@ -425,15 +445,17 @@ def main():
     # KG.movie_genre()
     # KG.movie_actor()
     # KG.movie_director('delete')
-    KG.simple_baseline()
+    KG.simple_baseline_for_revenue()
     print('dictionaries generated')
-#    KG.user_rating()
+    KG.evaluation()
+    print('evaluation finished')
+    #    KG.user_rating()
     # actors, directors = KG.delete_few_people()
     # print('Top 30 actors are: ', actors[:30])
     # print('Top 30 directors are: ', directors[:30])
-    KG.generate_HIN()
+    # KG.generate_HIN()
+
 
 if __name__ == "__main__":
     main()
-
 
